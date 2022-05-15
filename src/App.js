81cf1eb37e633b671d53e8371d3f8f902e1b94ad
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-05-15 12:48:45 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios'
+
+const url = 'https://v2.jokeapi.dev'
 
 function App() {
+  const [joke, setJoke] = useState('')
+  
+const getJoke = () => {
+  Axios.get(`${url}/joke/any`).then(res => {
+    console.log(res)
+    if (res.data.setup) {
+      return setJoke(`${res.data.setup} ... ${res.data.delivery}`)
+    }
+    return setJoke(res.data.joke)
+    
+  })
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Joke Fetcher</h1>
+    <button onClick={getJoke}>Get Joke</button>
+    <p>{joke}</p>
+
     </div>
   );
 }
